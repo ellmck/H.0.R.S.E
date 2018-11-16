ad563d56aa7afd69126dc814f8892a3a04a4f062
Elliott <elliottmckevitt@hotmail.co.uk>
2018-11-16 23:46:52 +0000

    more objects added

--- app.js
+++ app.js
@@ -1,211 +1,250 @@
-var hoop = document.getElementById("hoop"),
-scoreBoard = document.getElementsByClassName("score"),
-arrow = document.getElementById("arrow"),
-canvas = document.getElementById("canvas"),
-context = canvas.getContext("2d");
+"use strict";
 
-// Matter.js module aliases
-var Engine = Matter.Engine;
-    World = Matter.World;
-    Bodies = Matter.Bodies;
-    Body = Matter.Body;
-    Render = Matter.Render;
-    Constraint = Matter.Constraint,
+var Engine = Matter.Engine,
+World = Matter.World,
+Bodies = Matter.Bodies,
+Body = Matter.Body,
+Render = Matter.Render,
+Constraint = Matter.Constraint,
     Composites = Matter.Composites,
     Events = Matter.Events,
     MouseConstraint = Matter.MouseConstraint;
 
+// Matter.js module aliases
+var mouse;
+
+var canvas = document.createElement('canvas'),
+    context = canvas.getContext('2d');
+
+context.canvas.width  = window.innerWidth;
+context.canvas.height = window.innerHeight;
+
+var draw = function() {
+
 var render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        wireframes: false, // <-- important
-        background: "transparent"
-    }
-});
+        element: document.body,
+        engine: engine,
+        options: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            wireframes: false, // <-- important
+            background: "transparent"
+        }
+    });
 // create a Matter.js engine
 var engine = Engine.create(document.body, {
     render: render
 
 });
-engine.world.gravity.y = 4;
-var mouse = {
-    x: 0,
-    y: 0,
-    isDown: false
-};
+    engine.world.gravity.y = 4;
+   mouse = {
+        x: 0,
+        y: 0,
+        isDown: false
+    };
+    var hoop = document.getElementById("hoop"),
+        scoreBoard = document.getElementsByClassName("score"),
+        arrow = document.getElementById("arrow");
+
+    console.log(render);
+
 
 // add a mouse controlled constraint
-var mouseConstraint = MouseConstraint.create(engine);
+    var mouseConstraint = MouseConstraint.create(engine);
 
-Events.on(mouseConstraint, 'mouseup', function(event) {
-    mouseUp(event);
-});
-Events.on(mouseConstraint, 'mousemove', function(event) {
-    mouseMoved(event);
-});
-Events.on(mouseConstraint, 'mousedown', function(event) {
-    mouseDown(event);
-});
+    Events.on(mouseConstraint, 'mouseup', function (event) {
+        mouseUp(event);
+    });
+    Events.on(mouseConstraint, 'mousemove', function (event) {
+        mouseMoved(event);
+    });
+    Events.on(mouseConstraint, 'mousedown', function (event) {
+        mouseDown(event);
+    });
 
-var ballNetRatio = 2;
-var ballBackboardRatio = 4.4;
-var backboardOffsetRatio = 0.62;
 
-var ballDimensions = {
-    radius: 35,
-};
+    const ballNetRatio = 2;
+    const ballBackboardRatio = 3;
+    const backboardOffsetRatio = 0.62;
 
-var net = {
-    backboard: {
-        x: 0,
-        y: 0,
-        w: 30,
-        h: 0
-    },
-    rightRim: {
-        x: 0,
-        y: 0,
-        w: 8,
-        h: 8
-    },
-    leftRim: {
-        x: 0,
-        y: 0,
-        w: 8,
-        h: 8
-    },
-    backboardConnector: {
-        x: 0,
-        y: 0,
-        w: 4,
-        h: 4
-    }
-};
+    var ballDimensions = {
+        radius: 35,
+    };
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ballDimensions.radius = canvas.width * 0.02;
-net.backboard.h = (ballDimensions.radius * 2) * ballBackboardRatio;
-net.backboard.w = (ballDimensions.radius * 0.6);
+    var net = {
+        backboard: {
+            x: 0,
+            y: 0,
+            w: 30,
+            h: 0
+        },
+        rightRim: {
+            x: 0,
+            y: 0,
+            w: 8,
+            h: 8
+        },
+        leftRim: {
+            x: 0,
+            y: 0,
+            w: 8,
+            h: 8
+        },
+        backboardConnector: {
+            x: 0,
+            y: 0,
+            w: 4,
+            h: 4
+        }
+    };
 
-net.backboard.x = canvas.width - (canvas.width * 0.15);
-net.backboard.y = (canvas.height * 0.5) - net.backboard.h - ((ballDimensions.radius * 2) * backboardOffsetRatio);
-net.rightRim.x = net.backboard.x - net.rightRim.w - ((ballDimensions.radius * 2) * backboardOffsetRatio);
-net.rightRim.y = net.backboard.y  + ((ballDimensions.radius * 2) * backboardOffsetRatio);
-net.leftRim.x = net.rightRim.x - net.rightRim.w - ((ballDimensions.radius * 2) * ballNetRatio);
-net.leftRim.y = net.rightRim.y;
-net.backboardConnector.x = net.rightRim.x + net.rightRim.w;
-net.backboardConnector.w = net.backboard.x - net.rightRim.x;
-net.backboardConnector.y = net.rightRim.y;
-net.leftRim.y = net.rightRim.y;
+    ballDimensions.radius = canvas.width * 0.02;
+    net.backboard.h = (ballDimensions.radius * 2) * ballBackboardRatio;
+    net.backboard.w = (ballDimensions.radius * 0.4);
+    net.backboard.x = canvas.width - (canvas.width * 0.15);
+    net.backboard.y = (canvas.height * 0.4) - net.backboard.h - ((ballDimensions.radius * 2) * backboardOffsetRatio);
+    net.rightRim.x = net.backboard.x - net.rightRim.w - ((ballDimensions.radius * 2) * backboardOffsetRatio);
+    net.rightRim.y = net.backboard.y + ((ballDimensions.radius * 2) * backboardOffsetRatio);
+    net.leftRim.x = net.rightRim.x  - ((ballDimensions.radius * 2) * ballNetRatio);
+    net.leftRim.y = net.rightRim.y;
+    net.backboardConnector.w = net.rightRim.x - net.backboard.x ;
+    net.backboardConnector.x = net.rightRim.x - (net.backboardConnector.w *0.5);
+    net.backboardConnector.y = net.rightRim.y;
+    net.backboardConnector.h = (ballDimensions.radius * 0.25);
 
-hoop.style.left = net.leftRim.x + "px";
-hoop.style.top = (net.leftRim.y - 20) + "px";
-hoop.style.width = net.rightRim - net.leftRim + "px";
+    var backboardColor = "#127a81";
 
-var backboard = Bodies.rectangle(net.backboard.x, net.backboard.y, net.backboard.w, net.backboard.h, { isStatic: true, render: { fillStyle: '#6cff7c'} }),
-    backboardConnector = Bodies.rectangle(net.backboardConnector.x, net.backboardConnector.y, net.backboardConnector.w, net.backboardConnector.h, { isStatic: true, render: { fillStyle: '#6cff7c'} }),
-    backboardHoop = Bodies.rectangle(net.leftRim.x, net.leftRim.x, net.rightRim.x - net.leftRim.x, net.backboardConnector.h, { isStatic: true, render: { fillStyle: '#6cff7c'} });
+   var backboard = Bodies.rectangle(net.backboard.x, net.backboard.y, net.backboard.w, net.backboard.h, {
+            isStatic: true,
+
+            render: {fillStyle: backboardColor}
+        }),
+        backboardConnector = Bodies.rectangle(net.backboardConnector.x, net.backboardConnector.y, net.backboardConnector.w, net.backboardConnector.h, {
+            isStatic: true,
+            render: {fillStyle: backboardColor}
+        });
 
 // create game objects
-var ground = Bodies.rectangle(0, canvas.height, canvas.width * 2, 40, { isStatic: true, render: { visible: true } }),
-    rightWall = Bodies.rectangle( canvas.width, 0, 40, canvas.height * 2, { isStatic: true, render: { visible: true } }),
-    garage = Bodies.rectangle( 0, canvas.height, 40, canvas.height * 1.2, { isStatic: true, render: { visible: true } });
+    var ground = Bodies.rectangle(0, canvas.height, canvas.width * 2, (ballDimensions.radius * 2), {isStatic: true, render: { fillStyle: 'brown', visible: true}}),
+        rightWall = Bodies.rectangle(canvas.width, 0, (ballDimensions.radius * 2), canvas.height * 2, {isStatic: true, render: {fillStyle: 'black', visible: true}}),
+        garage = Bodies.rectangle(0, canvas.height, (ballDimensions.radius * 2), canvas.height * 1.2, {isStatic: true, render: {fillStyle: 'grey', visible: true}}),
+        roof = Bodies.rectangle(0-ballDimensions.radius * 12, 0, (ballDimensions.radius * 2), canvas.height * 1.2, {isStatic: true, render: {fillStyle: 'black', visible: true}});
+    Body.rotate(roof, -Math.PI/3);
 
-var ball = Matter.Bodies.circle(200, 200, ballDimensions.radius,  { density: 1, restitution: 0.9, friction: 0.1, render: { fillStyle: '#ff5f9f' } });
-var ballStartingX = 0;
-var ballStartingY = 0;
+    var rightRim = Matter.Bodies.circle(net.rightRim.x, net.rightRim.y, 2, {isStatic: true, render: {visible: true}}),
+        leftRim = Matter.Bodies.circle(net.leftRim.x, net.leftRim.y, 2, {isStatic: true, render: {visible: true}});
 
-var groupId = Body.nextGroup(),
-    particleOptions = { friction: 0.00001, groupId: groupId, render: { visible: false }},
-    cloth = Composites.softBody(net.leftRim.x, net.leftRim.y, 5, 4, 8, 2, false, 8, particleOptions);
+    var ball = Matter.Bodies.circle(200, 200, ballDimensions.radius, {
+        density: 1,
+        restitution: 0.9,
+        friction: 0.1,
+        render: {fillStyle: '#ff5f9f'}
+    });
+    var ballStartingX = 0;
+    var ballStartingY = 0;
 
-for (var i = 0; i < 5; i++) {
-    if (i === 0 || i  === 4)  {
-        cloth.bodies[i].isStatic = true;
-    }
-}
+    var leftBoxes = Composites.stack(net.leftRim.x, net.leftRim.y, 5, 1, (ballDimensions.radius * 0.5), 0,  function(x, y) {
+        return Bodies.rectangle(x, y, 6, 6);
+    });
+
+    var rightBoxes = Composites.stack(net.rightRim.x, net.rightRim.y, 5, 1, (ballDimensions.radius * 0.5), 0,  function(x, y) {
+        return Bodies.rectangle(x, y, 6, 6);
+    });
+
+    leftBoxes.bodies[0].isStatic = true;
+    rightBoxes.bodies[0].isStatic = true;
+
+
+    var leftChain = Composites.chain(leftBoxes, 0, 0, -0.5, 0, { stiffness: 1});
+    var rightChain = Composites.chain(rightBoxes, 0, 0, -0.5, 0, { stiffness: 1});
+
+    // for (var i = 0; i < 5; i++) {
+    //     if (i === 0 || i === 4) {
+    //         cloth.bodies[i].isStatic = true;
+    //     }
+    // }
 
 // add all of the bodies to the world
-World.add(engine.world, [ball, ground, rightWall, garage, mouseConstraint, cloth, backboard, backboardConnector, backboardHoop , hoop]);
+    World.add(engine.world, [ball, ground, rightWall, garage, roof, mouseConstraint, leftChain, rightChain, backboard, backboardConnector, leftRim, rightRim]);
 
 
-Events.on(engine, 'tick', function(event) {
+    Events.on(engine, 'tick', function (event) {
         if (ball.position.x > net.leftRim.x && ball.position.x < net.rightRim.x && ball.position.y > net.rightRim.y && ball.position.y < net.rightRim.y + 10) {
             scoreBoard[0].innerText = "YEET";
         }
+        else if( ball.position.y >= ground.position.y - ballDimensions.radius * 2 &&
+        scoreBoard[0].innerText === ""){
+            scoreBoard[0].innerText = "MISS";
+        }
+
     });
 
 
 // run the engine
 Engine.run(engine);
 
-window.onresize = function() {
-    // onResize(canvas)
-};
+// run the renderer
 
-function getMousePosition(e) {
-    mouse.x = e.mouse.position.x;
-    mouse.y = e.mouse.position.y;
-}
-
-function mouseMoved(e) {
-    if(!mouse.isDown){
-        return;
+    function getMousePosition(e) {
+        mouse.x = e.mouse.position.x;
+        mouse.y = e.mouse.position.y;
     }
-    getMousePosition(e)
-    Body.setPosition(ball, {
-        x:  mouse.x,
-        y:  mouse.y
-    });
-}
 
-var mouseDown = function(e) {
-    Body.setVelocity(ball, {
-        x: 0,
-        y: 0
-    })
-    getMousePosition(e)
-    mouse.isDown = true;
-    ballStartingX = mouse.x;
-    ballStartingY = mouse.y;
-    Body.setPosition(ball, {
-        x:  mouse.x,
-        y:  mouse.y
+    function mouseMoved(e) {
+        if (!mouse.isDown) {
+            return;
+        }
+        getMousePosition(e);
+        Body.setPosition(ball, {
+            x: mouse.x,
+            y: mouse.y
         });
-}
+    }
 
-var mouseUp = function(e) {
+    function mouseDown(e) {
+        scoreBoard[0].innerText = "";
+        Body.setVelocity(ball, {
+            x: 0,
+            y: 0
+        })
+        getMousePosition(e);
+        mouse.isDown = true;
+        ballStartingX = mouse.x;
+        ballStartingY = mouse.y;
+        Body.setPosition(ball, {
+            x: mouse.x,
+            y: mouse.y
+        });
+    }
+
+    function mouseUp(e) {
         mouse.isDown = false;
-    Body.setVelocity(ball, {
-        x: (ballStartingX - ball.position.x) * 0.2,
-        y: (ballStartingY - ball.position.y)* 0.2
-    })
-}
+        Body.setVelocity(ball, {
+            x: (ballStartingX - ball.position.x) * 0.2,
+            y: (ballStartingY - ball.position.y) * 0.2
+        })
+    }
 
-function getTouchPosition(e) {
-    mouse.x = e.mouse.position.x;
-    mouse.y = e.mouse.position.y;
-}
+    function getTouchPosition(e) {
+        mouse.x = e.mouse.position.x;
+        mouse.y = e.mouse.position.y;
+    }
 
-var touchDown = function(e) {
-    getTouchPosition(e);
-    mouse.isDown = true;
-    ball.position.x = mouse.x;
-    ball.position.y = mouse.y;
-    ballStartingX = mouse.x;
-    ballStartingY = mouse.y;
+    var touchDown = function (e) {
+        getTouchPosition(e);
+        mouse.isDown = true;
+        ball.position.x = mouse.x;
+        ball.position.y = mouse.y;
+        ballStartingX = mouse.x;
+        ballStartingY = mouse.y;
 
-}
-var touchUp = function(e) {
-    mouse.isDown = false;
-    ball.velocity.y = (ballStartingY - ball.position.y) * 0.1;
-    ball.velocity.x = (ballStartingX - ball.position.x) * 0.1;
+    }
+    var touchUp = function (e) {
+        mouse.isDown = false;
+        ball.velocity.y = (ballStartingY - ball.position.y) * 0.1;
+        ball.velocity.x = (ballStartingX - ball.position.x) * 0.1;
 
-}
+    }
 
 // window.onresize = function() {
 //     onResize(canvas)
@@ -369,10 +408,7 @@ var touchUp = function(e) {
 //
 // draw();
 //
-// var onResize = function() {
-//
-//     setInterval(draw(), 20000);
-// }
+
 //
 // function objectCollision(ball, rect) {
 //
@@ -415,3 +451,10 @@ var touchUp = function(e) {
 //
 //     return 0 < xDist * xVelocity + yDist * yVelocity;
 // }
+}
+
+draw();
+
+// window.onresize = function () {
+//     draw();
+// };
